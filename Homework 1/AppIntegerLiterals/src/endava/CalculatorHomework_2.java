f7784f3ec25f301fc7e4909c142f873f430a8708
Ana Acanski <Ana.Acanski@endava.com>
2019-12-09 16:05:35 +0100

    Adding files

---
+++ Homework 1/AppIntegerLiterals/src/endava/CalculatorHomework_2.java
@@ -0,0 +1,41 @@
+package endava;
+import java.util.Scanner;
+
+public class CalculatorHomework_2 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter an integer: ");
+		int number1 = input.nextInt();
+		System.out.println("You entered " + number1);
+		
+		System.out.println("Enter number: ");
+		int number2 = input.nextInt();
+		System.out.println("You entered " + number2);
+		
+		System.out.println("Enter number operation(+,-,*,/): 1 Sum, 2 Sub, 3 Mul, 4 Div");
+		int operation = input.nextInt();
+		
+		switch(operation) {
+		case 1:
+			System.out.println("Sum of entered numbers is:" + (number1+number2));
+			break;
+		case 2 :
+			System.out.println("Sub betwen entered numbers is:" + (number1-number2));
+			break;
+		case 3:
+			System.out.println("Mul for entered entered numbers is:" + (number1*number2));
+		    break;
+			case 4:
+				System.out.println("Div for entered numbers is:" + (number1/number2));
+				break;
+				default:
+					System.out.println("Illegal operation");
+		}
+	
+
+	}
+
+
+}
