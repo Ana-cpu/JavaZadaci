03dc0f657b93c98cbfed65c352a72f46510ba1f7
Your Name <you@example.com>
2020-01-14 13:29:01 +0100

    methodes are updated

--- Homework 1/Lesson4.1/src/hospital/Nurse.java
+++ Homework 1/Lesson4.1/src/hospital/Nurse.java
@@ -7,8 +7,44 @@ public class Nurse extends Employ{
 	private String shift;
 
 	public void nurseinformation(String name, String surname, String address, String phone, LocalDateTime date, String shift) {
-		super(name, surname, address, phone, shift);
+		this.name = name;
+		this.surname = surname;
+		this.address = address;
+		this.phone = phone;
+		this.date = date;
 		this.shift = shift;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+	
+	public void setSurnameame(String surname) {
+		this.surname = surname;
+	}
+	public String getSurame() {
+		return surname;
+	}
+	
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getAddress() {
+		return address;
+	}
+	
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	
+	public LocalDateTime setDate(LocalDateTime date) {
+		return this.date = date;
 	}
 	
 	public void setShift(String shift) {
