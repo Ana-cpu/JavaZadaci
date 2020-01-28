871934f74f264879b715af3eda766fc13ea3c6a2
Your Name <you@example.com>
2020-01-28 13:36:34 +0100

    modification according to reviewer's suggestion

--- Homework 1/Lesson3.2/src/nekretnina/Kuca.java
+++ Homework 1/Lesson3.2/src/nekretnina/Kuca.java
@@ -1,49 +1,25 @@
 package nekretnina;
 
-import nekretnina.Nekretnina.Vlasnik;
+import nekretnina.Vlasnik;
 
 public class Kuca extends Nekretnina{
+	String address;
+	String zone;
+	int kvadratura;
+	nekretnina.Vlasnik Vlasnik;
+	double okucnica;
 
 	
-	public void Kuca() {
-		adress = "Milutina Milankovica 9dj";
-		zone = "zone 1";
-		kvadratura = 1234567890;
-		okucnica = 25;
-	}
-	
-	public void Kuca(String adress, String zone, int kvadratura, Vlasnik Vlasnik, double okucnica) {
-		this.adress = adress;
+	public void Kuca(String address, String zone, int kvadratura, nekretnina.Vlasnik Vlasnik, double okucnica) {
+		this.address = address;
 		this.zone = zone;
 		this.kvadratura = kvadratura;
 		this.Vlasnik = Vlasnik;
 		this.okucnica = okucnica;
 		
 	}
-
 	
-	@Override
-	public String getKuca(String adress, String zone, int kvadratura, Vlasnik Vlasnik, double okucnica) {
-		return zone;
-	}
-
-	@Override
-	public String getAdress(String adress, String zone, int kvadratura, Vlasnik Vlasnik) {
-		return adress;
-	}
-
-	@Override
-	public String getKvadratura(String adress, String zone, int kvadratura, Vlasnik Vlasnik) {
-		return getKvadratura(null, null, 0, null);
-	}
-
-	@Override
-	public Vlasnik getVlasnik(String adress, String zone, int kvadratura, Vlasnik Vlasnik) {
-		return Vlasnik;
-	}
-
-	@Override
-	public double getOkucnica(String adress, String zone, int kvadratura, Vlasnik Vlasnik, double okucnica) {
-		return okucnica;
+	public String toString() {
+		return " Adresa: " + address + "\n Zona: " + zone + "\n Kvadratura: " + kvadratura + "\n Vlasnik: " + Vlasnik + "\n okucnica: " + okucnica;
 	}
 }
--- Homework 1/Lesson3.2/src/nekretnina/Stan.java
+++ Homework 1/Lesson3.2/src/nekretnina/Stan.java
@@ -1,43 +1,23 @@
 package nekretnina;
 
-import nekretnina.Nekretnina.Vlasnik;
+import nekretnina.Vlasnik;
 
 public class Stan extends Nekretnina{
+	String address;
+	String zone;
+	int kvadratura;
+	nekretnina.Vlasnik Vlasnik;
 
-	
-	public void Stan() {
-		adress = "Milutina Milankovica 9dj";
-		zone = "zone 1";
-		kvadratura = 1234567890;
-	}
-	
-	public void Stan(String adress, String zone, int kvadratura, Vlasnik Vlasnik) {
-		this.adress = adress;
+
+	public Stan(String string, String string2, int i, nekretnina.Vlasnik Vlasnik) {
+		this.address = address;
 		this.zone = zone;
 		this.kvadratura = kvadratura;
 		this.Vlasnik = Vlasnik;
-		
 	}
-
 	
-	@Override
-	public String getStan(String adress, String zone, int kvadratura, Vlasnik Vlasnik) {
-		return zone;
-	}
-
-	@Override
-	public String getAdress(String adress, String zone, int kvadratura, Vlasnik Vlasnik) {
-		return adress;
-	}
-
-	@Override
-	public String getKvadratura(String adress, String zone, int kvadratura, Vlasnik Vlasnik) {
-		return getKvadratura(null, null, 0, null);
-	}
-
-	@Override
-	public Vlasnik getVlasnik(String adress, String zone, int kvadratura, Vlasnik Vlasnik) {
-		return Vlasnik;
+	public String toString() {
+		return " Adresa: " + address + "\n Zona: " + zone + "\n Kvadratura: " + kvadratura + "\n Vlasnik: " + Vlasnik;
 	}
 
 }
