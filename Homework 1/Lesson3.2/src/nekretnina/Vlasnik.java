d6c04d097edd95833c3515fc36312df4d194227a
Your Name <you@example.com>
2020-01-28 12:30:34 +0100

    modification according to reviewer's suggestion

---
+++ Homework 1/Lesson3.2/src/nekretnina/Vlasnik.java
@@ -0,0 +1,21 @@
+package nekretnina;
+
+public class Vlasnik {
+	
+	String ime;
+	String prezime;
+	String jmbg;
+	String licnaKarta;
+	
+	public String toString() {
+		return " Ime: " + ime + "\n Prezime: " + prezime + "\n Maticni broj: " + jmbg + "\n Broj licne karte: " + licnaKarta;
+	}
+	
+	public Vlasnik(String ime, String prezime, String jmbg, String licnaKarta) {
+		this.ime = ime;
+		this.prezime = prezime;
+		this.jmbg = jmbg;
+		this.licnaKarta = licnaKarta;
+	}
+
+}
