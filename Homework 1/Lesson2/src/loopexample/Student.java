a4dd26b686308c19a7afc4c1f9bac6e38f90fbcc
Your Name <you@example.com>
2020-01-28 11:01:49 +0100

    modification according to reviewer's suggestion

--- Homework 1/Lesson2/src/loopexample/Student.java
+++ Homework 1/Lesson2/src/loopexample/Student.java
@@ -2,36 +2,44 @@ package loopexample;
 
 import java.util.Scanner;
 
+
 public class Student {
 
 	public static void main(String[] args) {
-		int length = 0;
-		String[] student = new String[length];
-		int points[] = new int[length];
-		int i;
-		Scanner input = new Scanner(System.in);
+		int length;
+
 		
 		System.out.println("Enter a number of students: ");
+		Scanner input = new Scanner(System.in);
 		length = input.nextInt();
+
+		String[] student = new String[length];
+		String temp_student = student[0];
+		int points[] = new int[length];
+		int i=0;
+		int temp = points[0];
 		
 		for(i=0; i<length; i++) {
 			System.out.println("Enter student: " + (i+1) + ":");
 			student[i] = input.next();
 			System.out.println("Enter points of student " + (i+1) + ":" );
 			points[i] = input.nextInt();
+			//student[i] = new Student();
 		}
+		
+
+		for(i=0; i<length-1; i++) {
+			//Calculate max points
+			if(points[i]<points[i+1]) {
+				temp = points[i+1];
+				temp_student = student[i+1];
+			}else
+				temp = points[i];
+			    temp_student = student[i+1];
+		}
+
+		System.out.println("Student with max points:  " + temp_student + "" + temp );
+		
 		input.close();
-		
-		//Calculate max points
-
-		int temp = points[0];
-		points[i] = points[0];
-		if(points[i]<points[i+1]) {
-			temp = points[i+1];
-			System.out.println("Student with max points:  " + temp);
-		}
-		
-
 	}
-
 }
