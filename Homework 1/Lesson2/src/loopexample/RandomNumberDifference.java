f0da1f7c6be1016c692f5c09128178809e1f4138
Ana Acanski <ana.acanski@endava.com>
2020-02-14 00:32:08 +0100

    possible solution according to reviewer's suggestion

--- Homework 1/Lesson2/src/loopexample/RandomNumberDifference.java
+++ Homework 1/Lesson2/src/loopexample/RandomNumberDifference.java
@@ -4,50 +4,56 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class RandomNumberDifference {
-	RandomNumberDifference(){
-	      //you need to remove input and calculation logic from constructor,
-	      //to avoid running it when constructing this object for testing.
-	}
-
-	 public int[] run() {
-      return getInput();
-  }
-	 
-	
-	
-	public static int[] getInput() {
-        Random rand = new Random();
+	public static void main(String[] args) {
+		Random rand = new Random();
 		
 		//Get Integer Input From the User
 		Scanner input = new Scanner(System.in);
+
+		String[] message = new String[2];
+		String[] unexpected = new String[1];
 		
-		System.out.print("Enter an integer between 1 and 50: ");
-		int number = input.nextInt();
-		System.out.println("You entered " + number);
+		message[0] = "Incredible, you enter expected number";
+		message[1]="Good catch, You were so good, difference is less then 5";
 		
-		//Generate random integer in range 0 to 50
-		int rand_int1 = rand.nextInt(50)+1;
-		
-		//Print random integer
-		System.out.println("Random Integer: "+rand_int1);
-		while( Math.abs(number-rand_int1)<=5) {
-			RandomNumberDifference random = new  RandomNumberDifference();
+		unexpected[0]="More lucky next time";
+
+		while(true) {
 			
-			//Incredible, you enter expected number
-			if(number == rand_int1) {
-				System.out.println("Incredible, you enter expected number");
-				random.run();
-			}else{
-				System.out.println("Good catch, You were so good:" + Math.abs(number-rand_int1));
-				random.run();
+			for(String current : message) {
+				System.out.println("Do you want to continue y or n: ");
+				String c = input.nextLine();
+
+				if(c.equalsIgnoreCase("n")){ 
+				    return;
+				}else {
+					
+					System.out.print("Enter an integer: ");
+					int number = input.nextInt();
+					System.out.println("You entered " + number);
+					
+					//Generate random integer in range 0 to 50
+					int rand_int1 = rand.nextInt(50);
+					
+					//Print random integer
+					System.out.println("Random Integer: "+rand_int1);
+					
+					//Incredible, you enter expected number
+					if(number == rand_int1) {
+						message[0] = "Incredible, you enter expected number";
+					}
+					int c1= (Math.abs(number-rand_int1));
+					if(c1>0 || c1 <=5) {
+						message[1]="Good catch, You were so good, difference is less then 5";
+					}
+					else{
+						unexpected[0]="More lucky next time";
+					}
+			     }
+			}
+			for(String current1 : unexpected) {
+				System.out.println(current1);
 			}
 		}
-		System.out.println("More lucky next time");
-		return null;
-	}
-
-	public static void main(String[] args) {
-		RandomNumberDifference random = new  RandomNumberDifference();
-	    random.run();
 	}
 }
