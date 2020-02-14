a2414710449a588366cc416a232b264e3943ea63
Your Name <you@example.com>
2020-02-14 14:32:26 +0100

    modification according reviewer's comment

--- Homework 1/Lesson3.2/src/nekretnina/Main.java
+++ Homework 1/Lesson3.2/src/nekretnina/Main.java
@@ -6,19 +6,23 @@ public class Main {
 		Nekretnina ne1 = new Stan();
 		Nekretnina ne2 = new Kuca();
 		
-
 		//Stan
 		System.out.println("Stan se nalazi na adresi: " + ne1.address);
 		System.out.println("Kvadratura stana je: " + ne1.kvadratura);
 		System.out.println("Stan se nalazi u zoni: " + ne1.zone);
-		
 		
 
 		//Kuca
 		
 		System.out.println("Kuca se nalazi na adresi: " + ne2.address);
 		System.out.println("Kvadratura kuce je:  " + ne2.kvadratura);
-		System.out.println("Zona ukojoj se kuca nalazi je: " + ne2.zone);
+		System.out.println("Zona u kojoj se kuca nalazi je: " + ne2.zone);
+		
+		//The apartment with a parameters
+		System.out.printf("Bulevar Patrijarha Pavla", 63, 2);
+		
+		//House with a parameters
+		System.out.printf("Bulevar Patrijarha Pavla", 63, 2);
 	}
 
 }
