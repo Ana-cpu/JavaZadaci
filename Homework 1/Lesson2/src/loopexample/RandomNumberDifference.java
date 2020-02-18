449084190f3aa9dc757280f7bf04dc824e6bcb35
Your Name <you@example.com>
2020-02-18 13:13:17 +0100

    better solution for loop

--- Homework 1/Lesson2/src/loopexample/RandomNumberDifference.java
+++ Homework 1/Lesson2/src/loopexample/RandomNumberDifference.java
@@ -7,75 +7,57 @@ public class RandomNumberDifference {
 	public static void test() {
 
         Random rand = new Random();
-		
-		//Get Integer Input From the User
 		Scanner input = new Scanner(System.in);
-
 		String[] message = new String[2];
 		String[] unexpected = new String[1];
-		
-		message[0] = "Incredible, you enter expected number";
-		message[1]="Good catch, You were so good, difference is less then 5";
-		
-		unexpected[0]="More lucky next time";
-		
 		System.out.print("Enter an integer: ");
 		int number = input.nextInt();
+		
+		if(number>50) {
+			System.out.println("Out of the rang, try again");
+			System.out.print("Enter an integer: ");
+			number = input.nextInt();
+		}
 		System.out.println("You entered " + number);
 		
 		//Generate random integer in range 0 to 50
 		int rand_int1 = rand.nextInt(50);
-		
-		//Print random integer
 		System.out.println("Random Integer: "+rand_int1);
 		
-		//Incredible, you enter expected number
 		if(number == rand_int1) {
 			message[0] = "Incredible, you enter expected number";
-		}
-		int c1= (Math.abs(number-rand_int1));
-		if(c1>0 || c1 <=5) {
-			message[1]="Good catch, You were so good, difference is less then 5";
+			System.out.println(message[0]);
 		}
 		else{
+
+			int c1= (Math.abs(number-rand_int1));
+			if(c1 <=5) {
+				message[1]="Good catch, You were so good, difference is less then 5";
+				System.out.println(message[1]);
+			}
+			else {
 			unexpected[0]="More lucky next time";
+			System.out.println(unexpected[0]);
+			}
 		}
 	}
 	public static void main(String[] args) {
-		 Random rand = new Random();
-		
-		//Get Integer Input From the User
 		Scanner input = new Scanner(System.in);
 
 		String[] message = new String[2];
 		String[] unexpected = new String[1];
-		
-		message[0] = "Incredible, you enter expected number";
-		message[1]="Good catch, You were so good, difference is less then 5";
-		
-		unexpected[0]="More lucky next time";
-		
 		System.out.println("Hello, this is a bingo program:) Press Enter and continue game:) ");
+		test();
 		String c = input.nextLine();
 		do{
 			test();
 			for(String current : message) {
-				System.out.println(current);
+				//System.out.println(current);
+				c = input.nextLine();
 				System.out.println("Do you want to continue y or n: ");
-				String c1 = input.nextLine();
-				System.out.println(current);
 			}
-			
-			for(String current1 : unexpected) {
-				System.out.println("You can run game again:)");
-				System.out.println(unexpected);
-				String c2 = input.nextLine();
-				break;
-			}
-			
-			}while(c.equalsIgnoreCase("n"));
-
-
+			}while(c.equalsIgnoreCase("Y"));
+		System.out.println("You finished your game. If you want to continue, please run application again.");
 		
 	}
 }
