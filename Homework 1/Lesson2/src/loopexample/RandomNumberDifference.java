9618443a67ffd0b8ec98bf7105c67772358a57c1
Your Name <you@example.com>
2020-02-19 10:24:42 +0100

    final solution for this task

--- Homework 1/Lesson2/src/loopexample/RandomNumberDifference.java
+++ Homework 1/Lesson2/src/loopexample/RandomNumberDifference.java
@@ -12,6 +12,7 @@ public class RandomNumberDifference {
 		String[] unexpected = new String[1];
 		System.out.print("Enter an integer: ");
 		int number = input.nextInt();
+		String c = "";
 		
 		if(number>50) {
 			System.out.println("Out of the rang, try again");
@@ -27,6 +28,7 @@ public class RandomNumberDifference {
 		if(number == rand_int1) {
 			message[0] = "Incredible, you enter expected number";
 			System.out.println(message[0]);
+			test();
 		}
 		else{
 
@@ -34,10 +36,13 @@ public class RandomNumberDifference {
 			if(c1 <=5) {
 				message[1]="Good catch, You were so good, difference is less then 5";
 				System.out.println(message[1]);
+				test();
 			}
 			else {
 			unexpected[0]="More lucky next time";
 			System.out.println(unexpected[0]);
+			System.out.println("You finished your game. If you want to continue, please run application again.");
+			return;
 			}
 		}
 	}
@@ -48,16 +53,6 @@ public class RandomNumberDifference {
 		String[] unexpected = new String[1];
 		System.out.println("Hello, this is a bingo program:) Press Enter and continue game:) ");
 		test();
-		String c = input.nextLine();
-		do{
-			for(String current : message) {
-				//System.out.println(current);
-				c = input.nextLine();
-				System.out.println("Do you want to continue y or n: ");
-			}
-			test();
-			}while(c.equalsIgnoreCase("Y"));
-		System.out.println("You finished your game. If you want to continue, please run application again.");
 		
 	}
 }
